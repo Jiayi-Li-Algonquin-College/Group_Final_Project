d2963682fcaeeb0daf1de9459b4fd516dc41da44
Ethan Jiayi Li <li000831@algonquinlive.com>
2023-07-13 11:40:46 -0400

    upload

--- app/src/main/java/algonquin/cst2335/group_final_project/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/group_final_project/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package algonquin.cst2335.group_final_project;
+package algonquin.cst2335.group_final_project.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import algonquin.cst2335.group_final_project.R;
 
 public class MainActivity extends AppCompatActivity {
 
