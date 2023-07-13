bbeb410ff18fe3d4eaddd00e644c6cbfabe1499d
Ethan Jiayi Li <li000831@algonquinlive.com>
2023-07-13 15:50:39 -0400

    upload

--- app/src/main/java/algonquin/cst2335/group_final_project/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/group_final_project/ui/MainActivity.java
@@ -3,14 +3,32 @@ package algonquin.cst2335.group_final_project.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.group_final_project.R;
+import algonquin.cst2335.group_final_project.databinding.ActivityMainBinding;
 
+/**
+ * MainActivity class represents an Android activity that allows the user to enter and check a password.
+ * It uses various methods to validate the password complexity and display feedback messages.
+ *
+ * @author Jiayi Li
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        TextView myText = variableBinding.myTextView;
+        Button myButton = variableBinding.myButton;
+
     }
 }
