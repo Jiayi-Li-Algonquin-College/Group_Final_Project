ed21e2e1dd5dfafda64f21d3fb37246d0804c526
si wang <wang0915@algonquinlive.com>
2023-07-18 19:31:58 -0400

    commit create GUI

---
+++ app/src/main/java/algonquin/cst2335/group_final_project/QuestionDatabase.java
@@ -0,0 +1,10 @@
+package algonquin.cst2335.group_final_project;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {QuizQuestion.class}, version = 1)
+public abstract class QuestionDatabase extends RoomDatabase {
+    //which DAO do we use for this database:
+    public abstract QuizQuestionDAO cmDAO();
+}
