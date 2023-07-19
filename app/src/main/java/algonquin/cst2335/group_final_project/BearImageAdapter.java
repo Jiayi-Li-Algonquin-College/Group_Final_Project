102fb3604bbf7b9d8625727afd37537a9457447f
Ethan Jiayi Li <li000831@algonquinlive.com>
2023-07-19 19:24:09 -0400

    除了bear都可以写了，我把bear相关的graddle的implementation都comment out了，bear的java也comment out了

--- app/src/main/java/algonquin/cst2335/group_final_project/BearImageAdapter.java
+++ app/src/main/java/algonquin/cst2335/group_final_project/BearImageAdapter.java
@@ -1,88 +1,39 @@
-package algonquin.cst2335.group_final_project;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.media.Image;
-import android.support.annotation.NonNull;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
-
-import java.util.List;
-
-import algonquin.cst2335.group_final_project.databinding.ActivityBearImageAdapterBinding;
-
-public class BearImageAdapter extends RecyclerView.Adapter<BearImageAdapter.ViewHolder> {
-
-    private final List<BearImage> images;
-
-    public BearImageAdapter(List<BearImage> images) {
-        this.images = images;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_bear_image_adapter, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Picasso.get().load(images.get(position).url).into(holder.imageView);
-    }
-
-    @Override
-    public int getItemCount() {
-        return images.size();
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
-
-        ViewHolder(View view) {
-            super(view);
-            imageView = view.findViewById(R.id.bearListImageView);
-        }
-    }
-}
-
-//new *
-//public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ImageViewHolder> {
-//    private final List<Image> images;
-//    private OnImageClickListener listener;
+//package algonquin.cst2335.group_final_project;
 //
-//    public ImageListAdapter(List<Image> images) {
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.media.Image;
+//import android.support.annotation.NonNull;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//
+//import com.squareup.picasso.Picasso;
+//
+//import java.util.List;
+//
+//import algonquin.cst2335.group_final_project.databinding.ActivityBearImageAdapterBinding;
+//
+//public class BearImageAdapter extends RecyclerView.Adapter<BearImageAdapter.ViewHolder> {
+//
+//    private final List<BearImage> images;
+//
+//    public BearImageAdapter(List<BearImage> images) {
 //        this.images = images;
-//    }
-//
-//    public interface OnImageClickListener {
-//        void onImageClick(Image image);
-//    }
-//
-//    public void setOnImageClickListener(OnImageClickListener listener) {
-//        this.listener = listener;
 //    }
 //
 //    @NonNull
 //    @Override
-//    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        ActivityBearImageAdapterBinding binding = ActivityBearImageAdapterBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-//        return new ImageViewHolder(binding);
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_bear_image_adapter, parent, false);
+//        return new ViewHolder(view);
 //    }
 //
 //    @Override
-//    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
-//        Image currentImage = images.get(position);
-//        holder.binding.bearListImageView.setImageBitmap(currentImage.getBitmap());
-//        holder.binding.deleteButton.setOnClickListener(v -> {
-//            if (listener != null) {
-//                listener.onImageClick(currentImage);
-//            }
-//        });
+//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        Picasso.get().load(images.get(position).url).into(holder.imageView);
 //    }
 //
 //    @Override
@@ -90,13 +41,63 @@ public class BearImageAdapter extends RecyclerView.Adapter<BearImageAdapter.View
 //        return images.size();
 //    }
 //
-//    class ImageViewHolder extends RecyclerView.ViewHolder {
-//        private final RecyclerviewItemBinding binding;
+//    static class ViewHolder extends RecyclerView.ViewHolder {
+//        ImageView imageView;
 //
-//        public ImageViewHolder(@NnNull ActivityBearImageAdapterBinding binding) {
-//            super(binding.getRoot());
-//            this.binding = binding;
+//        ViewHolder(View view) {
+//            super(view);
+//            imageView = view.findViewById(R.id.bearListImageView);
 //        }
 //    }
 //}
-// new **
+//
+////new *
+////public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ImageViewHolder> {
+////    private final List<Image> images;
+////    private OnImageClickListener listener;
+////
+////    public ImageListAdapter(List<Image> images) {
+////        this.images = images;
+////    }
+////
+////    public interface OnImageClickListener {
+////        void onImageClick(Image image);
+////    }
+////
+////    public void setOnImageClickListener(OnImageClickListener listener) {
+////        this.listener = listener;
+////    }
+////
+////    @NonNull
+////    @Override
+////    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+////        ActivityBearImageAdapterBinding binding = ActivityBearImageAdapterBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+////        return new ImageViewHolder(binding);
+////    }
+////
+////    @Override
+////    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+////        Image currentImage = images.get(position);
+////        holder.binding.bearListImageView.setImageBitmap(currentImage.getBitmap());
+////        holder.binding.deleteButton.setOnClickListener(v -> {
+////            if (listener != null) {
+////                listener.onImageClick(currentImage);
+////            }
+////        });
+////    }
+////
+////    @Override
+////    public int getItemCount() {
+////        return images.size();
+////    }
+////
+////    class ImageViewHolder extends RecyclerView.ViewHolder {
+////        private final RecyclerviewItemBinding binding;
+////
+////        public ImageViewHolder(@NnNull ActivityBearImageAdapterBinding binding) {
+////            super(binding.getRoot());
+////            this.binding = binding;
+////        }
+////    }
+////}
+//// new **
