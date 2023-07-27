ce5c8eb45f146ab9bd91dfbdb6a345b4cef657b1
Jiayi-Li-Algonquin-College <133148933+Jiayi-Li-Algonquin-College@users.noreply.github.com>
2023-07-27 13:24:29 -0400

    adds a bar

--- app/src/main/java/algonquin/cst2335/group_final_project/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/group_final_project/MainActivity.java
@@ -1,9 +1,14 @@
 package algonquin.cst2335.group_final_project;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import algonquin.cst2335.group_final_project.Yueying_Li_bear.BearImageMainActivity;
 import algonquin.cst2335.group_final_project.Jiebo_Peng_currency.CurrencyConvertActivity;
@@ -20,6 +25,37 @@ import algonquin.cst2335.group_final_project.databinding.ActivityMainBinding;
  */
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding variableBinding;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu_bar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.main_Item_1) {
+            Intent nextPage = new Intent( MainActivity.this, ChatRoom.class);
+            startActivity( nextPage);
+        } else if (itemId == R.id.main_Item_2) {
+//            Toast.makeText(getApplicationContext(), "This is a about message 😁" , Toast.LENGTH_SHORT).show();
+            Intent nextPage = new Intent( MainActivity.this, BearImageMainActivity.class);
+            startActivity( nextPage);
+        }else if (itemId == R.id.main_Item_3) {
+//            Toast.makeText(getApplicationContext(), "This is a about message 😁" , Toast.LENGTH_SHORT).show();
+            Intent nextPage = new Intent( MainActivity.this, TriviaActivity.class);
+            startActivity( nextPage);
+        }else if (itemId == R.id.main_item_4) {
+//            Toast.makeText(getApplicationContext(), "This is a about message 😁" , Toast.LENGTH_SHORT).show();
+            Intent nextPage = new Intent( MainActivity.this, CurrencyConvertActivity.class);
+            startActivity( nextPage);
+        }
+
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
 
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
+
+        setSupportActionBar(variableBinding.myToolbar);
 
         variableBinding.Plane.setOnClickListener( clk-> {
             Intent nextPage = new Intent( MainActivity.this, ChatRoom.class);
