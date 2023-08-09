224174727a5cbec13f9549e51ec679ae39022ef1
li000809 <129007049+li000809@users.noreply.github.com>
2023-08-09 16:48:14 -0400

    Update SavedImageActivity.java

--- app/src/main/java/algonquin/cst2335/group_final_project/SavedImageActivity.java
+++ app/src/main/java/algonquin/cst2335/group_final_project/SavedImageActivity.java
@@ -1,4 +1,4 @@
-package algonquin.cst2335.group_final_project;
+package algonquin.cst2335.group_final_project.Yueying_Li_bear;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,10 +10,23 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import algonquin.cst2335.group_final_project.R;
+
+/**
+ * This class maintains the saved images inside the RecyclerView
+ */
 public class SavedImageActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private BearImageDAO bearImageDAO;
 
+    /**
+     * creates the database of the image list
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +35,16 @@ public class SavedImageActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.BearRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // Initialize the database
         BearImageDatabase db = Room.databaseBuilder(getApplicationContext(), BearImageDatabase.class, "image").build();
+        bearImageDAO = db.bearImageDAO();
         new Thread(() -> {
             List<BearImage> images = db.bearImageDAO().getAllImages();
             runOnUiThread(() -> {
                 if (images.isEmpty()) {
                     Toast.makeText(SavedImageActivity.this, "No saved images", Toast.LENGTH_SHORT).show();
                 } else {
-                    recyclerView.setAdapter(new BearImageAdapter(images));
+                    recyclerView.setAdapter(new BearImageAdapter(images, bearImageDAO));
                 }
             });
         }).start();
