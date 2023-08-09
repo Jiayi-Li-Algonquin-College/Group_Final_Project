12af4774bb0d6e78ec1688e251c5d2aa5b039ff0
li000809 <129007049+li000809@users.noreply.github.com>
2023-08-09 16:42:22 -0400

    Update BearImageDAO.java

--- app/src/main/java/algonquin/cst2335/group_final_project/BearImageDAO.java
+++ app/src/main/java/algonquin/cst2335/group_final_project/BearImageDAO.java
@@ -1,4 +1,4 @@
-package algonquin.cst2335.group_final_project;
+package algonquin.cst2335.group_final_project.Yueying_Li_bear;
 
 import androidx.room.Dao;
 import androidx.room.Delete;
@@ -7,18 +7,36 @@ import androidx.room.Query;
 
 import java.util.List;
 
+/**
+ * This interface maintains the inserting, deleting one image, deleting all images and selecting all
+ * images from the database
+ */
 @Dao
 public interface BearImageDAO {
+    /**
+     * inserts a new image into database
+     * @param bearImage the object of BearImage
+     */
     @Insert
-    public static void insert(BearImage bearImage){
+    void insert(BearImage bearImage);
 
-    }
-
+    /**
+     * selects all images from database
+     * @return a list of images
+     */
     @Query("SELECT * FROM BearImage")
     List<BearImage> getAllImages();
 
+    /**
+     * deletes one image from database
+     * @param bearImage an object of bear image
+     */
     @Delete
-    public static void deleteBearImage(BearImage bearImage){
+    void deleteBearImage(BearImage bearImage);
 
-    }
+    /**
+     * deletes all images from database
+     */
+    @Query("DELETE FROM BearImage")
+    void deleteAllImages();
 }
