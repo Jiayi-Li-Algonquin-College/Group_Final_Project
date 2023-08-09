df229e535ace4ca2b1d4e9ce453981f0974daf05
Jiayi-Li-Algonquin-College <133148933+Jiayi-Li-Algonquin-College@users.noreply.github.com>
2023-08-09 17:38:37 -0400

    Update BearImageDatabase.java

--- app/src/main/java/algonquin/cst2335/group_final_project/Yueying_Li_bear/BearImageDatabase.java
+++ app/src/main/java/algonquin/cst2335/group_final_project/Yueying_Li_bear/BearImageDatabase.java
@@ -3,8 +3,15 @@ package algonquin.cst2335.group_final_project.Yueying_Li_bear;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+/**
+ * This abstract class maintains the database and the DAO
+ */
 @Database(entities = {BearImage.class}, version = 1)
 public abstract class BearImageDatabase extends RoomDatabase {
 
+    /**
+     * This abstract method is for DAO of bear image.
+     * @return a DAO of bear image
+     */
     public abstract BearImageDAO bearImageDAO();
 }
